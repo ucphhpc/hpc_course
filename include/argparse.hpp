7af9a40ee0bbb511bfe942c2a8886412b32d06d3
Mads R. B. Kristensen <madsbk@gmail.com>
2019-01-28 15:40:12 +0100

    util: implemented argparse

---
+++ include/argparse.hpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <string>
+
+namespace util {
+
+// from <https://stackoverflow.com/questions/865668/how-to-parse-command-line-arguments-in-c>
+class ArgParser {
+public:
+    ArgParser(int &argc, char **argv) {
+        for (int i = 1; i < argc; ++i)
+            this->tokens.push_back(std::string(argv[i]));
+    }
+
+    const std::string &getCmdOption(const std::string &option) const {
+        std::vector<std::string>::const_iterator itr;
+        itr = std::find(this->tokens.begin(), this->tokens.end(), option);
+        if (itr != this->tokens.end() && ++itr != this->tokens.end()) {
+            return *itr;
+        }
+        static const std::string empty_string("");
+        return empty_string;
+    }
+
+    bool cmdOptionExists(const std::string &option) const {
+        return std::find(this->tokens.begin(), this->tokens.end(), option)
+               != this->tokens.end();
+    }
+
+private:
+    std::vector<std::string> tokens;
+};
+
+} // namespace util
+
