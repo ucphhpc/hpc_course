0029c64ed6b57c1455f5312ba8ae343cb13383e5
Rasmus Munk <munk1@live.dk>
2020-01-20 14:55:00 +0100

    Fix singularity exec

--- module4/ShallowWater/slurm_job.sh
+++ module4/ShallowWater/slurm_job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mpirun singularity ~/modi_images/hpc-notebook-latest.simg ./sw_parallel --iter 100 --size 1024
+mpirun singularity exec ~/modi_images/hpc-notebook-latest.simg ./sw_parallel --iter 100 --size 1024
--- module5/CT_Reconstruction/slurm_job.sh
+++ module5/CT_Reconstruction/slurm_job.sh
@@ -4,5 +4,5 @@
 export OMP_NUM_THREADS=32
 
 # Run the program
-mpirun singularity ~/modi_images/hpc-notebook-latest.simg ./ct_parallel --num-voxels 256 --input ~/modi_mount/module5/CT_Reconstruction/ct_data
+mpirun singularity exec ~/modi_images/hpc-notebook-latest.simg ./ct_parallel --num-voxels 256 --input ct_data
 
