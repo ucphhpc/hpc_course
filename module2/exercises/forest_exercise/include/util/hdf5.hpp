3fd88dafd190e526da78a63c799d1f06d0ad3df6
Troels Haugboelle <haugboel@nbi.ku.dk>
2021-02-17 00:02:15 +0100

    RM remove old version of exercise

--- module2/exercises/forest_exercise/include/util/hdf5.hpp
+++
@@ -1,5 +0,0 @@
-#include <vector>
-#include <H5Cpp.h>
-
-void write_hdf5(H5::Group &group, const std::string &name, const std::vector <hsize_t> &shape,
-                const std::vector<double> &data);
--- module2/exercises/main.cpp
+++
@@ -1,7 +0,0 @@
-
-
-int main() {
-    //TODO, implement the III exercises here
-    // When reaching the forest exercises, implement your solution in the forest_exercise/main.cpp file
-    return 0;
-}
