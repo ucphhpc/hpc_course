de86f665d4289447372e5169ceeb5021cb2c18b3
Troels Haugboelle <haugboel@nbi.ku.dk>
2021-03-17 10:41:28 +0100

    Update singularity image name

--- module4/ShallowWater/slurm_job.sh
+++ module4/ShallowWater/slurm_job.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# You can change the number of cores per node and the number of nodes here
+# Total number of tasks:
+#SBATCH -n 1
+# Total number of nodes:
+#SBATCH -N 1
 
-mpirun singularity exec ~/modi_images/hpc-notebook-latest.simg ./sw_parallel --iter 100 --size 1024
+mpirun singularity exec ~/modi_images/hpc-notebook-latest.sif ./sw_parallel --iter 100 --size 1024
