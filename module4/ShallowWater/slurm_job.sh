1fdc4ebd109aa46d06289c4c8d8073f14de1a762
Troels Haugbølle <haugboel@nbi.ku.dk>
2022-02-08 08:46:26 +0100

    ORG cleaning repository for last years files

--- module4/ShallowWater/slurm_job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# You can change the number of cores per node and the number of nodes here
-# Total number of tasks:
-#SBATCH -n 1
-# Total number of nodes:
-#SBATCH -N 1
-
-mpirun singularity exec ~/modi_images/hpc-notebook-latest.sif ./sw_parallel --iter 100 --size 1024
