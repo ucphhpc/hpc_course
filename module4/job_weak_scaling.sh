7569162d681ce2692910e83a924ec534d566babd
Troels Haugboelle <haugboel@nbi.ku.dk>
2026-02-24 11:49:45 +0100

    Refine job scripts for week 4

--- module4/job_weak_scaling.sh
+++ module4/job_weak_scaling.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=Seismogram
 #SBATCH --partition=modi_HPPC
 #SBATCH --nodes=1 --ntasks=1 --threads-per-core=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=64
 #SBATCH --exclusive
 
 # set loop scheduling to static
@@ -14,6 +14,30 @@ export OMP_PLACES=cores
 
 # Place threads as close to each other as possible
 export OMP_PROC_BIND=close
+
+# Container path
+CONTAINER=~/modi_images/ucphhpc/hpc-notebook:latest
+
+# Loop through powers of 2 from 1 to 64 (change to from 2 to 128 if using hyperthreading)
+for OMP_NUM_THREADS in 1 2 4 8 16 32 64; do
+    echo
+    echo "=============================================="
+    echo " Running test with OMP_NUM_THREADS = $OMP_NUM_THREADS"
+    echo "=============================================="
+
+    export OMP_NUM_THREADS
+
+    # Compute NFREQ for this number of threads
+    nfreq=$(( 16384 * OMP_NUM_THREADS ))
+    echo "NFREQ = $nfreq"
+
+    # Clean + Build inside container
+    apptainer exec "$CONTAINER" make clean
+    apptainer exec "$CONTAINER" make NFREQ="$nfreq" mp
+
+    # Run the program
+    apptainer exec "$CONTAINER" ./mp
+done
 
 # Set and print number of threads
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
